a8e96e13e91ca08b39e206e8bf2e887d7085095d
scastagnoli <scastagnoli@users.noreply.github.com>
2025-12-11 16:59:58 +0100

    ultimo test

--- prova.py
+++ prova.py
@@ -2,3 +2,4 @@ tabellina = [x*2 for x in range(1,11)]
 print(tabellina)
 print("Modifica fatta dopo aver installato python3.... su pythonNode")
 print("Modifica dopo semplificazione, e va!")
+print("ultima prova")
